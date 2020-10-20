8e08360a0930b379323aa667f55b4deb8179cf5a
EduardoWhite <47611990+EduardoWhite@users.noreply.github.com>
2020-10-19 23:46:05 -0700

    Ejercicio 5

---
+++ ejercicio5.cpp
@@ -0,0 +1,24 @@
+/*
+ Numero de problema: 29.
+ Github: https://github.com/EduardoWhite
+ Creador: Blanco Melendez Manuel Eduardo
+ Fecha creacion: 18 de Octubre del 2020
+ Descripcion breve: Determinar el jugador de futbol mas popular
+ con respecto a unos votos realizados
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	
+	float tipoDolar = 20.3;
+	float dolares = 0;
+	float operacion = 0;
+	cout<<"Ingrese cantidad de dolares a convertir en pesos"<<endl;
+	cin>>dolares;
+	operacion = dolares*tipoDolar;
+	cout<<"El equivalente en pesos es: "<<operacion;
+	return 0;
+}
